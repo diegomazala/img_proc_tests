664c4606c0dbca6fd844f3cb01c6ba54f5c4c91f
AVELL_TVG\diego <diegomazala@gmail.com>
2017-07-07 22:36:25 -0300

    Inserted tif read write sample code

---
+++ src/tif_read_write.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include "opencv2/highgui.hpp"
+
+
+// work around conflicting definitions on 14.04 LTS with g++ 4.8.2
+# define uint64 uint64_hack_
+# define int64 int64_hack_
+
+#include <tiff.h>
+# undef uint64
+# undef int64
+# define uint64 uint64_hack_
+# define int64 int64_hack_
+
+
+
+int main(int argc, const char **argv)
+{
+	const std::string inFilename = argv[1];
+	const std::string outFilename = argv[2];
+
+	cv::Mat input = cv::imread(inFilename, cv::IMREAD_UNCHANGED);	// 18 2 3
+	if (input.empty())
+	{
+		std::cerr << "Cannot read image file: " << inFilename << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::cout << ' ' 
+		<< input.type() << ' '
+		<< input.depth() << ' '
+		<< input.channels() << std::endl;
+
+	cv::imwrite("../data/_out_io.tif", input);
+
+	int tags[] = { TIFFTAG_COMPRESSION, COMPRESSION_NONE };
+	bool success = cv::imwrite("../data/_out_COMPRESSION_NONE.tif", input, std::vector<int>(tags, tags + 2));
+
+	tags[1] = COMPRESSION_LZW;
+	success = cv::imwrite("../data/_out_COMPRESSION_LZW.tif", input, std::vector<int>(tags, tags + 2));
+
+	tags[1] = COMPRESSION_DEFLATE;
+	success = cv::imwrite("../data/_out_COMPRESSION_DEFLATE.tif", input, std::vector<int>(tags, tags + 2));
+
+	tags[1] = COMPRESSION_ADOBE_DEFLATE;
+	success = cv::imwrite("../data/_out_COMPRESSION_ADOBE_DEFLATE.tif", input, std::vector<int>(tags, tags + 2));
+
+
+	exit(0);
+	cv::waitKey();
+
+	const char* window_name = "Window";
+
+	cv::namedWindow(window_name, CV_WINDOW_NORMAL);
+	cv::resizeWindow(window_name, 720, 1280);
+	cv::moveWindow(window_name, 0, 0);
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
