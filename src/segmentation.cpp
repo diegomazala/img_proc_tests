c97f7f537061fb6e38c8ae460c03d66593fa9f63
AVELL_TVG\diego <diegomazala@gmail.com>
2017-07-11 00:05:44 -0300

    Segmentation running over folders

--- src/segmentation.cpp
+++ src/segmentation.cpp
@@ -25,91 +25,131 @@ static void show_window(const std::string& window_name, const cv::Mat& img)
 
 int main(int argc, char* argv[])
 {
+	int64 start_program_time = cv::getTickCount();
+
 	//check for the input parameter correctness
 	if (argc < 3) 
 	{
-		std::cerr << "Usage: app.exe <bg_image> <fg_image> <output_filename>" << std::endl;
+		std::cerr << "Usage: app.exe <bg_folder> <fg_folder> <output_folder>" << std::endl;
 		std::cerr << "Abort." << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	const std::string filename_bg = argv[1];
-	const std::string filename_fg = argv[2];
-	const std::string filename_output = (argc > 2) ? argv[3] : "segmentation_result.tif";
+	cv::String bg_path(argv[1]); 
+	cv::String fg_path(argv[2]);
+	cv::String out_path(argv[3]);
 
-	// read the image files in grayscale mode
-	cv::Mat frame_bg = cv::imread(filename_bg, CV_LOAD_IMAGE_GRAYSCALE);
-	cv::Mat frame_fg = cv::imread(filename_fg, CV_LOAD_IMAGE_GRAYSCALE);
-	cv::Mat frame_fg_rgb = cv::imread(filename_fg, CV_LOAD_IMAGE_UNCHANGED);
+	std::vector<cv::String> bg_files, fg_files;
+	cv::glob(bg_path, bg_files, true); // recurse
+	cv::glob(fg_path, fg_files, true); // recurse
 
-	if (frame_bg.empty() || frame_fg.empty())
+	const uint16_t image_count = std::min(bg_files.size(), fg_files.size());
+
+	for (int i = 0; i < image_count; ++i)
 	{
-		std::cerr << "Image could not be loaded. Abort" << std::endl;
-		return EXIT_FAILURE;
+		std::cout << "-------- " << i << "-------- " << std::endl;
+
+		int64 start_time = cv::getTickCount();
+
+		const std::string& filename_bg = bg_files[i];
+		const std::string& filename_fg = fg_files[i];
+
+		std::stringstream ss;
+		ss << out_path << "/out_" << i << ".tif";
+		const std::string filename_output = ss.str();
+
+		std::cout << "Reading input images ..." << std::endl;
+
+		//
+		// read the image files in grayscale mode
+		//
+		cv::Mat frame_bg = cv::imread(filename_bg, CV_LOAD_IMAGE_GRAYSCALE);
+		cv::Mat frame_fg = cv::imread(filename_fg, CV_LOAD_IMAGE_GRAYSCALE);
+		cv::Mat frame_fg_rgb = cv::imread(filename_fg, CV_LOAD_IMAGE_UNCHANGED);
+		//
+		if (frame_bg.empty() || frame_fg.empty())
+		{
+			std::cerr << "Image could not be loaded. Abort" << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		std::cout << "Subtracting background ..." << std::endl;
+
+		//
+		// image subtraction
+		//
+		cv::Mat frame_abs_diff;
+		cv::absdiff(frame_bg, frame_fg, frame_abs_diff);
+
+		cv::Mat frame_abs_gray;
+		if (frame_abs_diff.depth() != 0)
+			cv::cvtColor(frame_abs_diff, frame_abs_gray, CV_BGR2GRAY);
+		else
+			frame_abs_gray = frame_abs_diff;
+
+		std::cout << "Blurring image ..." << std::endl;
+
+		//
+		// smooth
+		//
+		cv::Mat smooth;
+		cv::medianBlur(frame_abs_gray, smooth, 25);
+
+		std::cout << "Threshold binary ..." << std::endl;
+
+		//
+		// binary threshold
+		//
+		cv::Mat binary_threshold;
+		const uint8_t bits_per_sample = (smooth.depth() == CV_16U || smooth.depth() == CV_16S) ? 16 : 8;
+		const uint32_t max_value = (uint32_t)pow(2, bits_per_sample);
+		cv::threshold(smooth, binary_threshold, float(max_value) * 0.1f, max_value, cv::THRESH_BINARY);
+
+		std::cout << "Floodfill ..." << std::endl;
+
+		//
+		// Floodfill from point (0, 0)
+		//
+		cv::Mat floodfill = binary_threshold.clone();
+		cv::floodFill(floodfill, cv::Point(0, 0), cv::Scalar(255));
+		//
+		// Invert floodfilled image
+		cv::Mat floodfill_inv;
+		bitwise_not(floodfill, floodfill_inv);
+		//
+		// Combine the two images to get the foreground.
+		cv::Mat floodfill_out = (binary_threshold | floodfill_inv);
+
+		//
+		// Apply mask to input fg image
+		//
+		cv::Mat segmentation;
+		frame_fg_rgb.copyTo(segmentation, floodfill_out);
+
+		std::cout << "Saving result " << i << " ..." << std::endl;
+
+		//
+		// Save result
+		//
+		bool saved = cv::imwrite(filename_output, segmentation);
+
+		if (saved)
+			std::cout << "Image " << i << " saved as " << filename_output << std::endl;
+		else
+			std::cout << "Error: Could not save image " << i << " : " << filename_output << std::endl;
+
+		//
+		// Time prints
+		//
+		int64 end_time = cv::getTickCount();
+		double time_diff = double(end_time - start_time) / cv::getTickFrequency();
+		std::cout << "Time spent in seconds :  " << time_diff << std::endl;
 	}
 
-
-	//
-	// image subtraction
-	//
-	cv::Mat frame_abs_diff;
-	cv::absdiff(frame_bg, frame_fg, frame_abs_diff);
-
-	cv::Mat frame_abs_gray;
-	if (frame_abs_diff.depth() != 0)
-		cv::cvtColor(frame_abs_diff, frame_abs_gray, CV_BGR2GRAY);
-	else
-		frame_abs_gray = frame_abs_diff;
-
-	//
-	// smooth
-	//
-	cv::Mat smooth;
-	cv::medianBlur(frame_abs_gray, smooth, 25);
-
-	//
-	// binary threshold
-	//
-	cv::Mat binary_threshold;
-	const uint8_t bits_per_sample = (smooth.depth() == CV_16U || smooth.depth() == CV_16S) ? 16 : 8;
-	const uint32_t max_value = (uint32_t)pow(2, bits_per_sample);
-	cv::threshold(smooth, binary_threshold, float(max_value) * 0.1f, max_value, cv::THRESH_BINARY);
-
-
-	//show_window("bg", frame_bg);
-	//show_window("fg", frame_fg);
-	//show_window("abs_diff", frame_abs_diff);
-	//show_window("binary_threshold", binary_threshold);
-	//show_window("smooth", smooth);
-
-
-	// Floodfill from point (0, 0)
-	cv::Mat im_floodfill = binary_threshold.clone();
-	floodFill(im_floodfill, cv::Point(0, 0), cv::Scalar(255));
-
-	// Invert floodfilled image
-	cv::Mat im_floodfill_inv;
-	bitwise_not(im_floodfill, im_floodfill_inv);
-
-	// Combine the two images to get the foreground.
-	cv::Mat im_out = (binary_threshold | im_floodfill_inv);
-
-	//show_window("im_floodfill", im_floodfill);
-	//show_window("im_floodfill_inv", im_floodfill_inv);
-	//show_window("im_out", im_out);
-
-	cv::Mat segmentation;
-	frame_fg_rgb.copyTo(segmentation, im_out);
-
-	show_window("segmentation", segmentation);
-
-	cv::waitKey(0);
-
-	cv::imwrite(filename_output, segmentation);
-	//cv::imwrite("../data/bg/bg_sub_abs.tif", frame_abs_diff);
-
-	//destroy GUI windows
-	cv::destroyAllWindows();
+	int64 end_program_time = cv::getTickCount();
+	std::cout 
+		<< "Total time in seconds :  " 
+		<< double(end_program_time - start_program_time) / cv::getTickFrequency() << std::endl;
 
 	return EXIT_SUCCESS;
 }
