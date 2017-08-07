4b2661b4a2d9de6649b78c20de981107aed2a592
POPEYE\diego <diegomazala@gmail.com>
2017-08-07 19:21:08 -0300

    Color fitting RGB WIP

--- src/color_fitting.h
+++ src/color_fitting.h
@@ -45,7 +45,7 @@ struct FittingFunctor : Functor<double>
 		{
 			//fvec(i) = this->Points[i][1] - (x(0) * this->Points[i][0] + x(1));
 			fvec(i) = this->target[i] - (x(0) * this->source[i] + x(1));
-			std::cout << fvec(i) << " : " << source[i] << ' ' << target[i] << std::endl;
+			//std::cout << fvec(i) << " : " << source[i] << ' ' << target[i] << std::endl;
 		}
 
 
@@ -71,9 +71,9 @@ struct RgbFitting
 	Eigen::Matrix<Type, Eigen::Dynamic, 3> target;
 	//Eigen::Matrix<double, 2, 1> result;
 
-	Eigen::Matrix<Type, 3, 1> functorResult;
+	Eigen::Matrix<Type, 3, 2> functorResult;
 
-
+	// LM minimize for the model y = a x + b
 	void compute()
 	{
 		//
@@ -81,15 +81,11 @@ struct RgbFitting
 		// 
 		for (int i = 0; i < 3; ++i)
 		{
-			Eigen::VectorXd result;
+			Eigen::VectorXd result(2);
 			result.fill(0.0f);
 			FittingNumericalDiff functor;
 			functor.source = source.col(i);	// per channel
 			functor.target = target.col(i);	// per channel
-
-			std::cout << "Size: " << functor.source.rows() << ' ' << functor.source.cols() << std::endl;
-			std::cout << "Size: " << functor.target.rows() << ' ' << functor.target.cols() << std::endl;
-
 			Eigen::LevenbergMarquardt<FittingNumericalDiff> lm(functor);
 			Eigen::LevenbergMarquardtSpace::Status status = lm.minimize(result);
 #if 1
@@ -97,15 +93,33 @@ struct RgbFitting
 			std::cout << "status: " << status << std::endl;
 			//std::cout << "info: " << lm.info() << std::endl;
 			std::cout << "x that minimizes the function: " << std::endl << result.transpose() << std::endl;
-			const auto src_channel = source.col(i);
-			const auto tgt_channel = target.col(i);
-			for (int j = 0; j < src_channel.rows(); ++j)
-			{
-				std::cout
-					<< std::fixed
-					<< src_channel[j] << ' ' << tgt_channel[j] << '\t' << src_channel[j] * result[0] + result[1] << std::endl;
-			}
+			//const auto src_channel = source.col(i);
+			//const auto tgt_channel = target.col(i);
+			//for (int j = 0; j < src_channel.rows(); ++j)
+			//{
+			//	std::cout
+			//		<< std::fixed
+			//		<< src_channel[j] << ' ' << tgt_channel[j] << '\t' << src_channel[j] * result[0] + result[1] << std::endl;
+			//}
 #endif
+			functorResult(i, 0) = result[0];
+			functorResult(i, 1) = result[1];
+		}
+
+
+		std::cout
+			<< "Functor Result: " << std::endl
+			<< functorResult << std::endl << std::endl;
+
+		for (int i = 0; i < source.rows(); ++i)
+		{
+			std::cout
+				<< std::fixed
+				<< source.row(i) << " <-> "
+				<< target.row(i) << "  =  "
+				<< source(i, 0) * functorResult(0, 0) + functorResult(0, 1) << ", "
+				<< source(i, 1) * functorResult(1, 0) + functorResult(1, 1) << ", "
+				<< source(i, 2) * functorResult(2, 0) + functorResult(2, 1) << std::endl;
 		}
 	}
 };
