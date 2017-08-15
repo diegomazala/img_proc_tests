7aa75de9e46f6f6cd0ce9e33eb2a37612a522f6f
AVELL_TVG\diego <diegomazala@gmail.com>
2017-08-14 23:17:05 -0300

    a few bat files

---
+++ bin/Release_x64/color_checker_detector.bat
@@ -0,0 +1,4 @@
+color_checker_detector.exe ^
+../../data/raw_tif/wFigurante_CAM07_10472.CR2.tiff ^
+../../data/raw_tif/Figurante_CAM07_10472.mat ^
+../../data/raw_tif/Figurante_CAM07_10472_detected.tif
